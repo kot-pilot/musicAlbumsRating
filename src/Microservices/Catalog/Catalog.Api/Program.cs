1d50c7c797f30461ffdfa12c96650855a71c21bc
Yauheni Lavitski <ylavitski@artezio.com>
2022-08-02 21:22:03 +0300

    add cors settings for blazor to be able to call web api on local dev environment

--- src/Microservices/Catalog/Catalog.Api/Program.cs
+++ src/Microservices/Catalog/Catalog.Api/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCorsPolicy", builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("DevCorsPolicy");
 }
 
 app.UseHttpsRedirection();
