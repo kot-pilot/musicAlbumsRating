0fb2fa84f74e0e4dae87f31857dd715505c78f9a
Yauheni Lavitski <ylavitski@artezio.com>
2022-09-06 21:21:16 +0300

    use base hosting env uri in case of relative uri string from settings; update catalog api url setting value;

--- src/BlazorUI/BlazorUI.App/Program.cs
+++ src/BlazorUI/BlazorUI.App/Program.cs
@@ -6,7 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string catalogApiUrl = builder.Configuration.GetValue<string>("CatalogApiUrl");
-builder.Services.AddScoped(sp => new CatalogApiClient { BaseAddress = new Uri(catalogApiUrl) });
+string catalogApiUrlSetting = builder.Configuration.GetValue<string>("CatalogApiUrl");
+Uri catalogApiUri = Uri.IsWellFormedUriString(catalogApiUrlSetting, UriKind.Absolute) ? new Uri(catalogApiUrlSetting) : new Uri(new Uri(builder.HostEnvironment.BaseAddress), catalogApiUrlSetting);
+
+builder.Services.AddScoped(sp => new CatalogApiClient { BaseAddress = new Uri(catalogApiUrlSetting) });
 
 await builder.Build().RunAsync();
